e2eade04a161888c62dcbcd2722344a89939e07a
Franknah <1145114@xbz.com>
2023-08-21 20:23:43 +0800

    更新ui

--- resource/ui/player_ui.py
+++ resource/ui/player_ui.py
@@ -16,10 +16,11 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QHBoxLayout,
-    QLabel, QLayout, QSizePolicy, QVBoxLayout,
-    QWidget)
+    QLabel, QLayout, QSizePolicy, QToolButton,
+    QVBoxLayout, QWidget)
 
-from qfluentwidgets import (PrimaryPushButton, Slider, SplitToolButton, TogglePushButton,
+from qfluentwidgets import (DropDownToolButton, PrimaryPushButton, PushButton, Slider,
+    SubtitleLabel, ToggleButton, ToolButton, TransparentDropDownToolButton,
     TransparentPushButton, TransparentToolButton)
 
 class Ui_Form(object):
@@ -29,14 +30,9 @@ class Ui_Form(object):
         Form.resize(549, 366)
         self.verticalLayout = QVBoxLayout(Form)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.label_lyric = QLabel(Form)
+        self.label_lyric = SubtitleLabel(Form)
         self.label_lyric.setObjectName(u"label_lyric")
-        self.label_lyric.setStyleSheet(u"QFrame:{\n"
-"border-width:1px;\n"
-"border-style: solid;\n"
-"border-color: black;\n"
-"border-radius: 8px;\n"
-"}")
+        self.label_lyric.setStyleSheet(u"")
         self.label_lyric.setFrameShape(QFrame.NoFrame)
         self.label_lyric.setFrameShadow(QFrame.Plain)
         self.label_lyric.setAlignment(Qt.AlignCenter)
@@ -46,21 +42,6 @@ class Ui_Form(object):
         self.gridLayout_3 = QGridLayout()
         self.gridLayout_3.setObjectName(u"gridLayout_3")
         self.gridLayout_3.setSizeConstraint(QLayout.SetDefaultConstraint)
-        self.horizontalSlider = Slider(Form)
-        self.horizontalSlider.setObjectName(u"horizontalSlider")
-        self.horizontalSlider.setMaximumSize(QSize(900, 22))
-        self.horizontalSlider.setOrientation(Qt.Horizontal)
-
-        self.gridLayout_3.addWidget(self.horizontalSlider, 2, 3, 1, 1)
-
-        self.labelTitle = QLabel(Form)
-        self.labelTitle.setObjectName(u"labelTitle")
-        font = QFont()
-        font.setBold(True)
-        self.labelTitle.setFont(font)
-
-        self.gridLayout_3.addWidget(self.labelTitle, 0, 2, 1, 3)
-
         self.labelLeft = QLabel(Form)
         self.labelLeft.setObjectName(u"labelLeft")
         self.labelLeft.setMinimumSize(QSize(0, 0))
@@ -72,27 +53,26 @@ class Ui_Form(object):
 
         self.gridLayout_3.addWidget(self.labelRight, 2, 4, 1, 1, Qt.AlignLeft)
 
+        self.horizontalSlider = Slider(Form)
+        self.horizontalSlider.setObjectName(u"horizontalSlider")
+        self.horizontalSlider.setMaximumSize(QSize(900, 22))
+        self.horizontalSlider.setOrientation(Qt.Horizontal)
+
+        self.gridLayout_3.addWidget(self.horizontalSlider, 2, 3, 1, 1)
+
+        self.labelTitle = SubtitleLabel(Form)
+        self.labelTitle.setObjectName(u"labelTitle")
+        font = QFont()
+        font.setBold(True)
+        self.labelTitle.setFont(font)
+
+        self.gridLayout_3.addWidget(self.labelTitle, 0, 2, 1, 3)
+
 
         self.verticalLayout.addLayout(self.gridLayout_3)
 
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
-        self.toolButtonVolumn = TransparentToolButton(Form)
-        self.toolButtonVolumn.setObjectName(u"toolButtonVolumn")
-        self.toolButtonVolumn.setAutoRaise(False)
-
-        self.gridLayout.addWidget(self.toolButtonVolumn, 2, 2, 1, 1, Qt.AlignRight)
-
-        self.toolButtonPlayMode = SplitToolButton(Form)
-        self.toolButtonPlayMode.setObjectName(u"toolButtonPlayMode")
-        self.toolButtonPlayMode.setProperty("checkable", False)
-        self.toolButtonPlayMode.setProperty("checked", False)
-        self.toolButtonPlayMode.setProperty("autoRepeat", False)
-        self.toolButtonPlayMode.setProperty("autoExclusive", False)
-        self.toolButtonPlayMode.setProperty("autoRaise", False)
-
-        self.gridLayout.addWidget(self.toolButtonPlayMode, 2, 3, 1, 1)
-
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.pushButtonPast = PrimaryPushButton(Form)
@@ -100,7 +80,7 @@ class Ui_Form(object):
 
         self.horizontalLayout.addWidget(self.pushButtonPast, 0, Qt.AlignRight)
 
-        self.pushButton = TogglePushButton(Form)
+        self.pushButton = ToggleButton(Form)
         self.pushButton.setObjectName(u"pushButton")
 
         self.horizontalLayout.addWidget(self.pushButton)
@@ -111,7 +91,29 @@ class Ui_Form(object):
         self.horizontalLayout.addWidget(self.pushButtonNext, 0, Qt.AlignLeft)
 
 
-        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 4)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 7)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.toolButtonDtLrc = TransparentToolButton(Form)
+        self.toolButtonDtLrc.setObjectName(u"toolButtonDtLrc")
+        self.toolButtonDtLrc.setPopupMode(QToolButton.DelayedPopup)
+
+        self.horizontalLayout_2.addWidget(self.toolButtonDtLrc)
+
+        self.toolButtonVolumn = TransparentToolButton(Form)
+        self.toolButtonVolumn.setObjectName(u"toolButtonVolumn")
+        self.toolButtonVolumn.setAutoRaise(False)
+
+        self.horizontalLayout_2.addWidget(self.toolButtonVolumn)
+
+        self.toolButtonPlayMode = TransparentDropDownToolButton(Form)
+        self.toolButtonPlayMode.setObjectName(u"toolButtonPlayMode")
+
+        self.horizontalLayout_2.addWidget(self.toolButtonPlayMode)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout_2, 2, 6, 1, 1)
 
         self.pushButtonLyric = TransparentPushButton(Form)
         self.pushButtonLyric.setObjectName(u"pushButtonLyric")
@@ -130,14 +132,14 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
         self.label_lyric.setText("")
-        self.labelTitle.setText("")
         self.labelLeft.setText(QCoreApplication.translate("Form", u"0\uff1a00", None))
         self.labelRight.setText(QCoreApplication.translate("Form", u"0\uff1a00", None))
+        self.labelTitle.setText("")
+        self.pushButtonPast.setText(QCoreApplication.translate("Form", u"\u4e0a\u4e00\u9996", None))
+        self.pushButton.setText(QCoreApplication.translate("Form", u"\u64ad\u653e", None))
+        self.pushButtonNext.setText(QCoreApplication.translate("Form", u"\u4e0b\u4e00\u9996", None))
         self.toolButtonVolumn.setText("")
-        self.toolButtonPlayMode.setProperty("text", QCoreApplication.translate("Form", u"...", None))
-        self.pushButtonPast.setProperty("text", QCoreApplication.translate("Form", u"\u4e0a\u4e00\u9996", None))
-        self.pushButton.setProperty("text", QCoreApplication.translate("Form", u"\u64ad\u653e", None))
-        self.pushButtonNext.setProperty("text", QCoreApplication.translate("Form", u"\u4e0b\u4e00\u9996", None))
+        self.toolButtonPlayMode.setText("")
         self.pushButtonLyric.setText(QCoreApplication.translate("Form", u"\u5207\u6362\u6b4c\u8bcd", None))
     # retranslateUi
 
