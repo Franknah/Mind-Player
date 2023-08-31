f6573292359c26087d1cb7a465583f7e3dcaa8f1
Franknah <1145114@xbz.com>
2023-08-31 13:22:00 +0800

    新增翻译

---
+++ resource/i18n/setting_interface.zh_CN.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en_US">
+<context>
+    <name>SettingInterface</name>
+    <message>
+        <location filename="setting_interface.py" line="31"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="34"/>
+        <source>Music on this PC</source>
+        <translation>此 PC 上的音乐</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="37"/>
+        <source>Local music library</source>
+        <translation>本地音乐库</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="43"/>
+        <location filename="setting_interface.py" line="215"/>
+        <source>Choose folder</source>
+        <translation>选择文件夹</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="45"/>
+        <source>Download directory</source>
+        <translation>下载目录</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="52"/>
+        <source>Personalization</source>
+        <translation>个性化</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="57"/>
+        <source>Application theme</source>
+        <translation>应用主题</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="58"/>
+        <source>Change the appearance of your application</source>
+        <translation>调整你的应用外观</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="60"/>
+        <source>Light</source>
+        <translation>浅色</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="60"/>
+        <source>Dark</source>
+        <translation>深色</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="62"/>
+        <location filename="setting_interface.py" line="78"/>
+        <source>Use system setting</source>
+        <translation>跟随系统设置</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="67"/>
+        <source>Mica</source>
+        <translation>云母效果</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="68"/>
+        <source>Turn on Mica effect in Mind-Player</source>
+        <translation>在 Mind-Player 启用云母效果</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="76"/>
+        <source>Language</source>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="77"/>
+        <source>Set your preferred language for UI</source>
+        <translation>选择界面所使用的语言</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="88"/>
+        <source>Main Panel</source>
+        <translation>主面板</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="91"/>
+        <source>Minimize to tray after closing</source>
+        <translation>关闭后最小化到托盘</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="92"/>
+        <source>PyQt-Fluent-Widgets will continue to run in the background</source>
+        <translation>Mind-Player 将在后台运行</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="99"/>
+        <source>Software update</source>
+        <translation>软件更新</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="102"/>
+        <source>Check for updates when the application starts</source>
+        <translation>在应用程序启动时检查更新</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="103"/>
+        <source>The new version will be more stable and have more features</source>
+        <translation>新版本将更加稳定并具有更多功能</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="109"/>
+        <location filename="setting_interface.py" line="129"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="112"/>
+        <source>Open help page</source>
+        <translation>打开帮助页面</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="114"/>
+        <source>Help</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="116"/>
+        <source>Discover new features and learn useful tips about Mind Player</source>
+        <translation>发现新功能并了解有关 Mind-Player 的使用技巧</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="120"/>
+        <location filename="setting_interface.py" line="122"/>
+        <source>Provide feedback</source>
+        <translation>提供反馈</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="123"/>
+        <source>Help us improve PyQt-Fluent-Widgets by providing feedback</source>
+        <translation>通过提供反馈帮助我们改进 Mind-Player</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="127"/>
+        <source>Check update</source>
+        <translation>检查更新</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="130"/>
+        <source>Copyright</source>
+        <translation>版权所有</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="131"/>
+        <source>Version</source>
+        <translation>当前版本</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="135"/>
+        <source>Open config file</source>
+        <translation>打开配置文件</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="137"/>
+        <source>Config</source>
+        <translation>配置</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="138"/>
+        <source>Open config file to configure</source>
+        <translation>打开配置文件以自行配置</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="201"/>
+        <source>Configuration takes effect after restart</source>
+        <translation>配置将在重启后生效</translation>
+    </message>
+    <message>
+        <location filename="setting_interface.py" line="208"/>
+        <source>Choose font</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="setting_interface.py" line="270"/>
+        <source>PySide6-Fluent-Widgets</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
